fdd9ce670d55d720dbd9ebb3a7c3b952a7a8d8e8
yasha0x80 <yasha0x80@gmail.com>
2013-08-08 09:29:48 +0300

    initial comit for asteriods

---
+++ asteroids/app/layers/menu_layer.rb
@@ -0,0 +1,6 @@
+class MenuLayer < Joybox::Core::Layer
+  def on_enter
+    # background_sprite = Sprite.new file_name: 'sprites/menu/background.png', position: Screen.center
+    # self << background_sprite
+  end
+end
---
+++ asteroids/app/scenes/game_scene.rb
@@ -0,0 +1,6 @@
+class GameScene < Joybox::Core::Scene
+  def on_enter
+    game_layer = GameLayer.new
+    self << game_layer
+  end
+end
